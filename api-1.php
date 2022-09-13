74dd2d13016378dbe1e73d2f81d9f6f76f427286
“Housam <housam.kak20@gmail.com>
2022-09-13 08:26:23 +0300

    Api-1 Done

--- api-1.php
+++ api-1.php
@@ -1,7 +1,20 @@
 <?php
 
+// Initialize the boolean variable to false
+$isPalindrome=FALSE;  
+// Get the string from the url
 $string=$_GET['string'];
-$response=array("name"=>$name);
+// Find the reverse string
+$revString=strrev($string);
+// Check if the string and its reverse are equal
+if($string==$revString)
+{
+    // Upon equality change the boolean to true
+    $isPalindrome=TRUE;
+}
+// put the result in an array
+$response=array("is_palindrome"=>$isPalindrome);
+// return the json object
 echo json_encode($response);
 
 ?>
